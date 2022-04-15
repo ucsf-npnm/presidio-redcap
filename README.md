8c5fd62cf58633bd086b300fd09352d264fafc8d
Ankit Khambhati <akhambhati@gmail.com>
2022-04-14 22:56:34 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Tests](https://github.com/ScaangosLab/presidio-redcap/workflows/Tests/badge.svg)](https://github.com/ScangosLab/presidio-redcap/actions?workflow=Tests)
+[![Read the Docs](https://readthedocs.org/projects/presidio-redcap/badge/)](https://presidio-redcap.readthedocs.io/)
 
 # presidio-redcap
 
--- noxfile.py
+++ noxfile.py
@@ -106,6 +106,14 @@ def docs(session: Session) -> None:
     session.run("sphinx-build", "docs", "docs/_build")
 
 
+@nox.session(python="3.8")
+def coverage(session: Session) -> None:
+    """Upload coverage data."""
+    install_with_constraints(session, "coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
+
+
 @nox.session(python=["3.10"])
 def tests(session: Session) -> None:
     """Run unit-tests."""
